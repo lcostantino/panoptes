6c2c37994b4a648417f59d9f3963e77b81aaf42e
Leandro Costantino <lcostantino@gmail.com>
2022-11-24 19:07:10 -0500

    Fix lock

--- panoptes/simple_cache.go
+++ panoptes/simple_cache.go
@@ -1,6 +1,8 @@
 package panoptes
 
-import "sync"
+import (
+	"sync"
+)
 
 const MAX_ITEMS int = 2000
 
@@ -11,8 +13,8 @@ type CacheEvent struct {
 
 func (c *CacheEvent) AddEvent(jsonData []byte) {
 	c.mLock.Lock()
-
 	defer c.mLock.Unlock()
+
 	if len(c.data) > MAX_ITEMS {
 		return
 	}
@@ -23,11 +25,11 @@ func (c *CacheEvent) GetCopyAndClean() [][]byte {
 
 	var dataCopy [][]byte
 	c.mLock.Lock()
-      if len(c.data) == 0 { 
-          return [][]byte{}
-      }
+	defer c.mLock.Unlock()
+	if len(c.data) == 0 {
+		return [][]byte{}
+	}
 	dataCopy = append(dataCopy, c.data...)
 	c.data = [][]byte{}
-	c.mLock.Unlock()
 	return dataCopy
 }
